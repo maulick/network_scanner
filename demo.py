1505f087537cc38c91216356702df31fc59e7bb9
Maulick Metkar <44560123+maulick@users.noreply.github.com>
2020-08-01 21:39:08 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,6 +3,7 @@ import scapy.all as scapy
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     arp_request.show()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     
     scapy.arping(ip)
 
